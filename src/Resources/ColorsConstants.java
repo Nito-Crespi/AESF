b8f77a2bed62c10dcb89c6b00756a71caeb4dad2
Nito.Crespi <63932237+Nito-Crespi@users.noreply.github.com>
2023-11-24 21:06:52 -0300

    Create ColorsConstants.java

---
+++ src/Resources/ColorsConstants.java
@@ -0,0 +1,16 @@
+package Resources;
+
+import java.awt.Color;
+
+public class ColorsConstants {
+    public static final Color PRIMARY_TEXT_COLOR_BLACK  = new Color(88, 88, 88);
+    public static final Color SECONDARY_TEXT_COLOR_BLACK  = new Color(173,173,173);
+    // public static final Color PRIMARY_TEXT_COLOR_GRAY  = new Color(173,173,173);
+    // public static final Color SECONDARY_TEXT_COLOR_GRAY  = new Color(88, 88, 88);
+    public static final Color PRIMARY_COLOR_WHITE  = new Color(250,250,250);
+    public static final Color SECONDARY_COLOR_WHITE  = new Color(235,235,235);
+    public static final Color PRIMARY_BACKGROUND_COLOR_YELLOW  = new Color(255,190,55);
+    //public static final Color SECONDARY_BACKGROUND_COLOR_YELLOW  = new Color(235,235,235);
+    public static final Color PRIMARY_COLOR_PURPLE  = new Color(65,45,195);
+    public static final Color SECONDARY_COLOR_PURPLE  = new Color(142,127,224);
+}
